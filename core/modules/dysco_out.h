b23a6ac25f8113dbb7c734a4bf17333f3e6f4fd7
fabricioufmt <fabricio.ufmt@gmail.com>
2017-10-31 21:24:47 -0300

    DyscoOut

---
+++ core/modules/dysco_out.h
@@ -0,0 +1,34 @@
+#ifndef BESS_MODULES_DYSCOOUT_H_
+#define BESS_MODULES_DYSCOOUT_H_
+
+#include "../module.h"
+#include "../pb/module_msg.pb.h"
+#include "../port.h"
+#include "../worker.h"
+
+class DyscoOut final : public Module {
+ public:
+  static const gate_idx_t kNumIGates = MAX_GATES;
+  static const gate_idx_t kNumOGates = 0;
+
+  DyscoOut() : Module(), port_(), available_queues_(), worker_queues_() {}
+
+  CommandResponse Init(const bess::pb::PortOutArg &arg);
+
+  void DeInit() override;
+
+  void ProcessBatch(bess::PacketBatch *batch) override;
+
+  int OnEvent(bess::Event e) override;
+
+  std::string GetDesc() const override;
+
+ private:
+  Port *port_;
+
+  std::vector<queue_t> available_queues_;
+
+  int worker_queues_[Worker::kMaxWorkers];
+};
+
+#endif  // BESS_MODULES_DYSCOOUT_H_
