b3a8076fdb303b564a96b0647c623ad3d9a672af
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-04 20:58:47 -0300

    .

--- core/modules/dysco_tcp_filter.cc
+++ core/modules/dysco_tcp_filter.cc
@@ -1,8 +1,9 @@
 #include "dysco_tcp_filter.h"
 
 void DyscoTcpFilter::ProcessBatch(bess::PacketBatch* batch) {
-  bpf.igates = this->igates_;
-  bpf.ogates = this->ogates_;
+  bpf.igates() = igates_;
+  bpf.ogates() = ogates_;
+  batch = NULL;
 }
 
 
