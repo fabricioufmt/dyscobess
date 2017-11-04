81199abcc890b080b9917a4108d886a9dcda7a20
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-04 20:26:56 -0300

    .

---
+++ core/modules/dysco_tcp_filter.cc
@@ -0,0 +1,8 @@
+#include "dysco_tcp_filter.h"
+
+void DyscoTcpFilter::ProcessBatch(bess::PacketBatch* batch) {
+
+}
+
+
+ADD_MODULE(DyscoTcpFilter, "dysco_tcp_filter", "classifies packet as TCP or non-TCP")
---
+++ core/modules/dysco_tcp_filter.h
@@ -0,0 +1,24 @@
+#ifndef BESS_MODULES_DYSCOTCPFILTER_H_
+#define BESS_MODULES_DYSCOTCPFILTER_H_
+
+#include <pcap.h>
+
+#include <vector>
+
+#include "../module.h"
+#include "../pb/module_msg.pb.h"
+#include "bpf.h"
+
+class DyscoTcpFilter final : public Module {
+ public:
+  static const gate_idx_t kNumIGates = 1;
+  static const gate_idx_t kNumOGates = 2;
+  
+ DyscoTcpFilter() : Module() {}
+  
+  void ProcessBatch(bess::PacketBatch*) override;
+ private:
+  BPF::Filter filter;
+};
+
+#endif // BESS_MODULES_DYSCOTCPFILTER_H_
