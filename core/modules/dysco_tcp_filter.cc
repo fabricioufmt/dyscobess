4c0a4929d2d4dcd1cf8ca230bbcf9171fe2493f3
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-04 22:47:51 -0300

    .

--- core/modules/dysco_tcp_filter.cc
+++ core/modules/dysco_tcp_filter.cc
@@ -17,18 +17,49 @@ DyscoTcpFilter::DyscoTcpFilter() : Module() {
   bpf.Init(arg);
 
   /*
-    Disconnect gates
+    Disconnect BPF gates
    */
+  bpf.DisconnectModules(0);
+  bpf.DisconnectModules(1);
+  bpf.DisconnectModulesUpstream(0); 
+  
+  /*
+    Disconnect and new connect ogate 0
+   */
+  bess::OGate* ogate = ogates()[0];
+  bess::Module* m_next = ogate->next();
+  gate_idx_t igate_idx = ogate->igate_idx();
+  DisconnectModules(0);
+  bpf.ConnectModules(0, m_next, igate_idx);
+
+  ogate = ogates()[1];
+  m_next = ogate->next();
+  igate_idx = ogate->igate_idx();
+  DisconnectModules(1);
+  bpf.ConnectModules(1, m_next, igate_idx);
+  /*
+
+
+
+
+
+  
   bess::IGate* igate = igates()[0];
   bess::OGate* ogate1 = ogates()[0];
+
+  
+
+  
   bess::OGate* ogate2 = ogates()[1];
 
+  
+  
   DisconnectModules(0);
   DisconnectModules(1);
   DisconnectModulesUpstream(0);
   bpf.DisconnectModules(0);
   bpf.DisconnectModules(1);
-  bpf.DisconnectModulesUpstream(0);  
+  bpf.DisconnectModulesUpstream(0);  */
 }
 
 void DyscoTcpFilter::ProcessBatch(bess::PacketBatch* batch) {
