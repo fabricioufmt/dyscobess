752fac3875574e29238ae74725694e3f93408a89
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-05 16:10:38 -0300

    .

--- core/modules/dysco_policycenter.h
+++ core/modules/dysco_policycenter.h
@@ -1,6 +1,8 @@
 #ifndef BESS_MODULES_DYSCOPOLICYCENTER_H_
 #define BESS_MODULES_DYSCOPOLICYCENTER_H_
 
+#include "../module.h"
+
 class DyscoPolicyCenter final : public Module {
  public:
   static const gate_idx_t kNumIGates = 1;
