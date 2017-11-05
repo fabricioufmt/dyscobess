1d5bf0474f86ef55d34260400b6f562647594773
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-05 16:57:06 -0300

    .

---
+++ core/modules/dysco_nonsyn.cc
@@ -0,0 +1,10 @@
+#include "dysco_nonsyn.h"
+
+DyscoNonSyn::DyscoNonSyn() : Module() {
+}
+
+void DyscoNonSyn::ProcessBatch(bess::PacketBatch* batch) {
+  RunChooseModule(0, batch);
+}
+
+ADD_MODULE(DyscoNonSyn, "dysco_nonsyn", "processes TCP NON-SYN fragment")
