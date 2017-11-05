c864b2fd155508381a20b24d935479751ea7c5c6
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-04 21:54:59 -0300

    .

--- core/modules/dysco_tcp_filter.cc
+++ core/modules/dysco_tcp_filter.cc
@@ -1,11 +1,16 @@
 #include "dysco_tcp_filter.h"
 
 DyscoTcpFilter::DyscoTcpFilter() : Module() {
-  //const bess::pb::BPFArg arg;
-  //arg.filters.filter = "ip and tcp";
-  //arg.filters.priority = 64;
-  bpf.add(filters=[{"priority":8, "filter": "ip and tcp", "gate": 1}]);
-  bpf.add(filters=[{"priority":4, "filter": "not ip or not tcp", "gate": 0}])
+  bess::pb::BPFArg arg;
+  bess::pb::BPFArg::Filter f1, f2;
+  f1.set_priority(8);
+  f1.set_filter("ip and tcp");
+  f1.set_gate(0);
+  f2.set_priority(4);
+  f2.set_filter("not ip or not tcp");
+  f2.set_gate(1);
+
+  bess::pb::BPFArg::Filter* filters = arg.filters();
 }
 
 void DyscoTcpFilter::ProcessBatch(bess::PacketBatch* batch) {
