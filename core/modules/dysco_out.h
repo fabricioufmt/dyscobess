eb1039d932418ef328faa7f02924a38aa171c355
Ronaldo Alves Ferreira <raf@facom.ufms.br>
2017-11-06 11:33:55 -0300

    Testing commit privileges

--- core/modules/dysco_out.h
+++ core/modules/dysco_out.h
@@ -8,27 +8,27 @@
 
 class DyscoOut final : public Module {
  public:
-  static const gate_idx_t kNumIGates = MAX_GATES;
-  static const gate_idx_t kNumOGates = 0;
-
-  DyscoOut() : Module(), port_(), available_queues_(), worker_queues_() {}
-
-  CommandResponse Init(const bess::pb::DyscoOutArg &arg);
-
-  void DeInit() override;
-
-  void ProcessBatch(bess::PacketBatch *batch) override;
-
-  int OnEvent(bess::Event e) override;
-
-  std::string GetDesc() const override;
-
+	static const gate_idx_t kNumIGates = MAX_GATES;
+	static const gate_idx_t kNumOGates = 0;
+	
+ DyscoOut() : Module(), port_(), available_queues_(), worker_queues_() {}
+	
+	CommandResponse Init(const bess::pb::DyscoOutArg &arg);
+	
+	void DeInit() override;
+	
+	void ProcessBatch(bess::PacketBatch *batch) override;
+	
+	int OnEvent(bess::Event e) override;
+	
+	std::string GetDesc() const override;
+	
  private:
-  Port *port_;
-
-  std::vector<queue_t> available_queues_;
-
-  int worker_queues_[Worker::kMaxWorkers];
+	Port *port_;
+	
+	std::vector<queue_t> available_queues_;
+	
+	int worker_queues_[Worker::kMaxWorkers];
 };
 
 #endif  // BESS_MODULES_DYSCOOUT_H_
