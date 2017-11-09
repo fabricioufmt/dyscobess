711eefa0f8d8a1b46af3bdbe8de5a1e78a4175fd
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-09 15:37:23 -0300

    .

--- core/modules/dysco_policycenter.cc
+++ core/modules/dysco_policycenter.cc
@@ -12,4 +12,8 @@ DyscoPolicyCenter::DyscoPolicyCenter() : Module() {
 
 }
 
+bool DyscoPolicyCenter::add(Ipv4* ip, Tcp* tcp, uint8_t* payload, int payload_len) {
+	return true;
+}
+
 ADD_MODULE(DyscoPolicyCenter, "dysco_policycenter", "Dysco core")
