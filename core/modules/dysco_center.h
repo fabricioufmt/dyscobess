e75a4cc01abe454058ff62d5ab701aeecec3296d
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-11 14:43:44 -0300

    .

--- core/modules/dysco_center.h
+++ core/modules/dysco_center.h
@@ -57,7 +57,7 @@ class DyscoCenter final : public Module {
 	bool add_mapping(Ipv4*, Tcp*, uint8_t*, uint32_t);
 	bool add_policy_rule(uint32_t, std::string, uint8_t*, uint32_t);
 	DyscoTcpSession* get_supss(Ipv4*, Tcp*);
-	DyscoControlBlock* get_controlblock(bess::Packet*);
+	DyscoControlBlock* get_controlblock(Ipv4*, Tcp*);
 	DyscoTcpSession* get_nextss(Ipv4*, Tcp*);
 	DyscoBPF::Filter* get_filter(bess::Packet*);
 	
