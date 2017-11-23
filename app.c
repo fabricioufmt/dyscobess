976b68fd4e2b63837c653c905f558bdf1b588be9
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-23 19:20:01 -0300

    .

--- app.c
+++ app.c
@@ -12,6 +12,9 @@ int main(int argc, char** argv) {
 	char buffer[65535];
 	struct sockaddr saddr;
 
+	char mac_src[6];
+	char mac_dst[6];
+
 	i = 0;
 	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
 	if(sockfd < 0)
@@ -24,6 +27,11 @@ int main(int argc, char** argv) {
 		n = recvfrom(sockfd, buffer, sizeof(buffer), 0, &saddr, (socklen_t*) &saddr_size);
 		if(n < 0)
 			continue;
+
+		mac_dst = buffer;
+		mac_src = buffer + 6;
+
+		printf("Src: %s Dst: %s\n", mac_src, mac_dst);
 
 		printf("Packet counter: %d.\n", i++);
 		n = sendto(sockfd, buffer, n, 0, &saddr, (socklen_t) saddr_size);
