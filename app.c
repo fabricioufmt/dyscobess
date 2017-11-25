378d45aece23ccbe5865f9ffc0b1e0a867da871b
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-25 08:22:12 -0300

    .

--- app.c
+++ app.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <linux/if_ether.h>
 
 int main(int argc, char** argv) {
 	int i;
@@ -12,11 +13,9 @@ int main(int argc, char** argv) {
 	char buffer[65535];
 	struct sockaddr saddr;
 
-	char mac_src[6];
-	char mac_dst[6];
-
 	i = 0;
-	sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	//sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+	sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 	if(sockfd < 0)
 		return -1;
 
@@ -25,18 +24,17 @@ int main(int argc, char** argv) {
 		memset(buffer, 0, sizeof(buffer));
 		
 		n = recvfrom(sockfd, buffer, sizeof(buffer), 0, &saddr, (socklen_t*) &saddr_size);
-		if(n < 0)
+		if(n < 0) {
+			perror("recvfrom < 0 byte");
 			continue;
-
-		mac_dst = buffer;
-		mac_src = buffer + 6;
-
-		printf("Src: %s Dst: %s\n", mac_src, mac_dst);
-
+		}
+		
 		printf("Packet counter: %d.\n", i++);
 		n = sendto(sockfd, buffer, n, 0, &saddr, (socklen_t) saddr_size);
-		if(n < 0)
+		if(n < 0) {
+			perror("sendto < 0 byte");
 			continue;
+		}
 	}
 
 	return 0;
