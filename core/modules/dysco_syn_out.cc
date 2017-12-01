8303cc50660e6191c2c3bd4b4d21531aee91166d
fabricioufmt <fabricio.ufmt@gmail.com>
2017-11-30 23:45:34 -0300

    .

--- core/modules/dysco_syn_out.cc
+++ core/modules/dysco_syn_out.cc
@@ -42,6 +42,7 @@ bool DyscoSynOut::process_packet(bess::Packet* pkt) {
 		fprintf(stderr, "DyscoSynOut: filter is NULL\n");
 	else {
 		fprintf(stderr, "DyscoSynOut: filter is not NULL\n");
+		fprintf(stderr, "sc_len: %d\nsc: ", filter->sc_len);
 		for(uint32_t i = 0; i < filter->sc_len; i++)
 			fprintf(stderr, "%x", filter->sc[i]);
 		fprintf(stderr, "\n");
