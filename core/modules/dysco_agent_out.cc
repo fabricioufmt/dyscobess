de2f689199febe2dae03b16b5caf83762c7b5f9a
fabricioufmt <fabricio.ufmt@gmail.com>
2017-12-03 23:09:27 -0300

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -24,7 +24,7 @@ CommandResponse DyscoAgentOut::Init(const bess::pb::DyscoAgentOutArg& arg) {
 }
 
 bool DyscoAgentOut::process_packet(bess::Packet* pkt) {
-	const uint8_t* metadata = pkt->metadata<uint8_t*>();
+	const uint8_t* metadata = pkt->metadata<char*>();
 	fprintf(stderr, "DyscoAgentOut(metadata): %c\n", metadata[0]);
 	
 	return true;
