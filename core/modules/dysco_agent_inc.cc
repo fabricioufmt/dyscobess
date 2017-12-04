06b7ce149a07ed0c3d1c9ad91e263cad06c256d5
fabricioufmt <fabricio.ufmt@gmail.com>
2017-12-03 21:47:45 -0300

    .

---
+++ core/modules/dysco_agent_inc.cc
@@ -0,0 +1,42 @@
+#include "dysco_agent_inc.h"
+#include "../module_graph.h"
+
+DyscoAgentInc::DyscoAgentInc() : Module() {
+	dc = 0;
+	index = 0;
+}
+
+CommandResponse DyscoAgentInc::Init(const bess::pb::DyscoAgentIncArg& arg) {
+	if(!arg.dc().length())
+		return CommandFailure(EINVAL, "'dc' must be given as string.");
+
+	const auto& it = ModuleGraph::GetAllModules().find(arg.dc().c_str());
+	if(it == ModuleGraph::GetAllModules().end())
+		return CommandFailure(ENODEV, "Module %s not found.", arg.dc().c_str());
+
+	dc = reinterpret_cast<DyscoCenter*>(it->second);
+	if(!dc)
+		return CommandFailure(ENODEV, "DyscoCenter module is NULL.");
+
+	index = dc->get_index(this->name());
+	
+	return CommandSuccess();
+}
+
+bool DyscoAgentInc::process_packet(bess::Packet* pkt) {
+	return true;
+}
+
+void DyscoAgentInc::ProcessBatch(bess::PacketBatch* batch) {
+	int cnt = batch->cnt();
+
+	bess::Packet* pkt = 0;
+	for(int i = 0; i < cnt; i++) {
+		pkt = batch->pkts()[i];
+		process_packet(pkt);
+	}
+	
+	RunChooseModule(0, batch);
+}
+
+ADD_MODULE(DyscoAgentInc, "dysco_agent_inc", "processes packets incoming")
