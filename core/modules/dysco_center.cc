31c88359de286a52b0003476cad31e2375697195
fabricioufmt <fabricio.ufmt@gmail.com>
2017-12-18 23:40:30 -0300

    .

--- core/modules/dysco_center.cc
+++ core/modules/dysco_center.cc
@@ -296,37 +296,31 @@ DyscoHashIn* DyscoCenter::insert_cb_in(uint32_t i, Ipv4* ip, Tcp* tcp, uint8_t* 
 			return 0;
 		}
 	} else {
-		fprintf(stderr, "insert_cb_in_reverse\n");
 		cb_out = insert_cb_in_reverse(ss, ip, tcp);
 		if(!cb_out) {
 			delete cb_in;
-			fprintf(stderr, "insert_cb_in_reverse is NULL\n");
 			return 0;
 		}
-		fprintf(stderr, "insert_cb_in_reverse is not NULL\n");	
 			
 		cb_in->set_cb_out(cb_out);
 		cb_out->set_cb_in(cb_in);
 	}
 	
-	fprintf(stderr, "insert_pending is not NULL\n");
 	fprintf(stderr, "cb_in (adding on hash_in: SUB):\n");
 	fprintf(stderr, "(SUB)%s:%u -> %s:%u\n",
-		printip0(ntohl(sub->sip)), ntohs(sub->sport),
-		printip0(ntohl(sub->dip)), ntohs(sub->dport));
+		printip0(ntohl(cb_in->get_sub()->sip)), ntohs(cb_in->get_sup()->sport),
+		printip0(ntohl(cb_in->get_sub()->dip)), ntohs(cb_in->get_sub()->dport));
 	fprintf(stderr, "(SUP)%s:%u -> %s:%u\n",
-		printip0(ntohl(sup->sip)), ntohs(sup->sport),
-		printip0(ntohl(sup->dip)), ntohs(sup->dport));
-	dh->hash_in.insert(std::pair<DyscoTcpSession, DyscoHashIn>(*sub, *cb_in));
+		printip0(ntohl(cb_in->get_sup()->sip)), ntohs(cb_in->get_sup()->sport),
+		printip0(ntohl(cb_in->get_sup()->dip)), ntohs(cb_in->get_sup()->dport));
+	dh->hash_in.insert(std::pair<DyscoTcpSession, DyscoHashIn>(*cb_in->get_sub(), *cb_in));
 
 	if(cb_out) {
-		fprintf(stderr, "[index: %u] cb_out (adding on out: SUP):\n", i);
-		fprintf(stderr, "[index: %u]: (SUB)%s:%u -> %s:%u\n",
-			i,
+		fprintf(stderr, "cb_out (adding on out: SUP):\n");
+		fprintf(stderr, "(SUB)%s:%u -> %s:%u\n",
 			printip0(ntohl(cb_out->get_sub()->sip)), ntohs(cb_out->get_sub()->sport),
 			printip0(ntohl(cb_out->get_sub()->dip)), ntohs(cb_out->get_sub()->dport));
-		fprintf(stderr, "[index: %u]: (SUP)%s:%u -> %s:%u\n",
-			i,
+		fprintf(stderr, "(SUP)%s:%u -> %s:%u\n",
 			printip0(ntohl(cb_out->get_sup()->sip)), ntohs(cb_out->get_sup()->sport),
 			printip0(ntohl(cb_out->get_sup()->dip)), ntohs(cb_out->get_sup()->dport));
 
