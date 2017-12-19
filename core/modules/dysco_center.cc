ef290b70964993c44975d423a6286cfb0706e321
fabricioufmt <fabricio.ufmt@gmail.com>
2017-12-19 10:53:17 -0300

    .

--- core/modules/dysco_center.cc
+++ core/modules/dysco_center.cc
@@ -471,7 +471,8 @@ bool DyscoCenter::process_pending_packet(uint32_t i, bess::Packet* pkt, Ipv4* ip
 		fprintf(stderr, "(SUP)%s:%u -> %s:%u\n",
 			printip0(ntohl(cb_in->get_sup()->sip)), ntohs(cb_in->get_sup()->sport),
 			printip0(ntohl(cb_in->get_sup()->dip)), ntohs(cb_in->get_sup()->dport));
-		dh->hash_in.insert(std::make_pair(*cb_in->get_sub(), *cb_in));	
+		//dh->hash_in.insert(std::make_pair(*cb_in->get_sub(), *cb_in));
+		dh->hash_in.insert(std::pair<DyscoTcpSession, DyscoHashIn>(*cb_in->get_sub(), *cb_in));
 	}
 
 	/*LIST FOR TEST*/
