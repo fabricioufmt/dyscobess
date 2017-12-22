1133b97be804238db423860fd15ec65cd34ea9c9
fabricioufmt <fabricio.ufmt@gmail.com>
2017-12-21 21:30:24 -0300

    .

--- core/modules/dysco_center.cc
+++ core/modules/dysco_center.cc
@@ -178,25 +178,8 @@ DyscoHashOut* DyscoCenter::lookup_output_pending(uint32_t i, Ipv4* ip, Tcp* tcp)
 	return 0;
 }
 
-DyscoHashOut* DyscoCenter::lookup_pending_tag(uint32_t i, Ipv4* ip, Tcp* tcp) {
-	DyscoHashes* dh = get_hash(i);
-	if(!dh)
-		return 0;
-
-	DyscoTcpSession ss;
-	ss.sip = htonl(ip->src.value());
-	ss.dip = htonl(ip->dst.value());
-	ss.sport = htons(tcp->src_port.value());
-	ss.dport = htons(tcp->dst_port.value());
-	
-	DyscoTcpSessionEqualTo equals;
-	unordered_map<DyscoTcpSession, DyscoHashPenTag, DyscoTcpSessionHash>::iterator it = dh->hash_pen_tag.begin();
-	while(it != dh->hash_pen_tag.end()) {
-		if(equals((*it).first, ss))
-			return &(*it).second;
-		it++;
-	}
-
+DyscoHashOut* DyscoCenter::lookup_pending_tag(uint32_t, Ipv4*, Tcp*) {
+	//TODO
 	return 0;
 }
 
