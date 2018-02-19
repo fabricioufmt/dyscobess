0cbb1e4aec95e0bf5d2a9aa6adc6b68dd30c7b5e
fabricioufmt <fabricio.ufmt@gmail.com>
2018-02-19 06:48:16 -0400

    .

--- core/drivers/dysco_vport.h
+++ core/drivers/dysco_vport.h
@@ -10,6 +10,7 @@ class DyscoVPort final : public Port {
  public:
  DyscoVPort() : fd_(), bar_(), map_(), netns_fd_(), container_pid_() {
 		//Dysco
+		devip = 1;
 		//memset(ns, 0, sizeof(ns));
 	}
 	void InitDriver() override;
