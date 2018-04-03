9fbf26e08b71153f1e2ed6dacff3fd5bd53dddd2
fabricioufmt <fabricio.ufmt@gmail.com>
2018-04-03 10:15:11 -0400

    .

--- dysco_ctl_sender.c
+++ dysco_ctl_sender.c
@@ -47,7 +47,7 @@ int main(int argc, char** argv) {
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(INADDR_ANY);
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv_addr.sin_port = htons(PORT);
 
 	if(connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
