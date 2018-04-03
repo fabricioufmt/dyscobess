8a76a8ee4f86a65229307e56c833a1c6b62d8b54
fabricioufmt <fabricio.ufmt@gmail.com>
2018-04-03 13:44:07 -0400

    .

---
+++ heart-cli.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define BUFFSIZE 4
+#define SEED 100
+
+int main(int argc, char** argv) {
+	int n;
+	int sockfd;
+	int connfd;
+	int heartvalue;
+	socklen_t addr_len;
+	char buff[BUFFSIZE];
+	struct sockaddr_in serv_addr;
+
+	if(argc != 3) {
+		fprintf(stderr, "Usage: %s <ip_address> <port>.\n", argv[0]);
+
+		return EXIT_FAILURE;
+	}
+	
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket failed");
+		return EXIT_FAILURE;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_addr.sin_port = htons(atoi(argv[2]));
+
+	if(connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+		perror("connect failed");
+		return EXIT_FAILURE;
+	}
+
+	srand(SEED);
+	heartvalue = rand();
+	memset(buff, 0, BUFFSIZE);
+	memcpy(buff, &heartvalue, sizeof(int));
+	
+	while(1) {
+		write(sockfd, buff, sizeof(int));
+		read(sockfd, buff, sizeof(int));
+		(*(int*)buff)++;
+		sleep(2);
+	}
+	
+	return 0;
+}
---
+++ heart-srv.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define LISTENQ 100
+#define BUFFSIZE 4
+#define LISTENPORT 9090
+
+int main(int argc, char** argv) {
+	int n;
+	int sockfd;
+	int connfd;
+	pid_t pid;
+	int heartvalue;
+	socklen_t addr_len;
+	char buff[BUFFSIZE];
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in conn_addr;
+
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket failed");
+		return EXIT_FAILURE;
+	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(LISTENPORT);
+
+	if(bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
+		perror("bind failed");
+		return EXIT_FAILURE;
+	}
+
+	if(listen(sockfd, LISTENQ) == -1) {
+		perror("listen failed");
+		return EXIT_FAILURE;
+	}
+
+	while(1) {
+		memset(&conn_addr, 0, sizeof(struct sockaddr_in));
+		if((connfd = accept(sockfd, (struct sockaddr*) &conn_addr, &addr_len)) == -1) {
+			perror("accept failed");
+			close(sockfd);
+			
+			return EXIT_FAILURE;
+		}
+
+		pid = fork();
+		if(pid > 0) {
+			close(connfd);
+			continue;
+		} else if (pid == 0) {
+			memset(buff, 0, BUFFSIZE);
+
+			while(1) {
+				read(connfd, buff, BUFFSIZE);
+				(*((int*)buff))++;
+				write(connfd, buff, BUFFSIZE);
+			}
+
+			close(connfd);
+		} else {
+			close(connfd);
+			continue;
+		}
+	}
+
+	close(sockfd);
+	
+	return 0;
+}
