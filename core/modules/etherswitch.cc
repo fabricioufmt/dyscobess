799d5772e8ac20724e429fd2e29f3dc5b4e8f6bb
fabricioufmt <fabricio.ufmt@gmail.com>
2018-04-11 11:32:03 -0400

    .

--- core/modules/etherswitch.cc
+++ core/modules/etherswitch.cc
@@ -61,5 +61,5 @@ void EtherSwitch::ProcessBatch(bess::PacketBatch* batch) {
 	}
 
 	for(uint32_t i = 0; i < ngates_; i++)
-		RunChooseModule(ogates()[i], &(out_gates[i]));
+		RunChooseModule(i, &(out_gates[i]));
 }
