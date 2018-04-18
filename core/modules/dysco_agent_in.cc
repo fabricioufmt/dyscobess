0d42c0b6aab70e8decd2f6a9c194170b399c4f9b
fabricioufmt <fabricio.ufmt@gmail.com>
2018-04-17 22:28:39 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -168,7 +168,7 @@ void DyscoAgentIn::ProcessBatch(bess::PacketBatch* batch) {
 					set_ack_number_out(this->index, tcp, cb_in);
 					in_hdr_rewrite_csum(ip, tcp, cb_in);
 #ifdef DEBUG
-					fprintf(stderr, "[%s][DyscoAgentIn] is TCP+SYN.\n",, ns.c_str());
+					fprintf(stderr, "[%s][DyscoAgentIn] is TCP+SYN.\n", ns.c_str());
 #endif
 				} else {
 					//It is retransmission packet, just remove sc (if there is) and insert Dysco Tag
