d36d5a3ef07d65234b3a032fd44e60c1df164484
fabricioufmt <fabricio.ufmt@gmail.com>
2018-04-23 13:47:32 -0400

    .

--- core/modules/dysco_center.cc
+++ core/modules/dysco_center.cc
@@ -1231,7 +1231,11 @@ bool DyscoCenter::insert_hash_reconfig(uint32_t i, DyscoCbReconfig* rcb) {
 	}
 	
 	DyscoTcpSession* ss = &rcb->super;
-	dh->hash_reconfig.insert(std::pair<DyscoTcpSession, DyscoCbReconfig>(*ss, *rcb));
+	bool ok = dh->hash_reconfig.insert(std::pair<DyscoTcpSession, DyscoCbReconfig>(*ss, *rcb)).second;
+	if(ok)
+		fprintf(stderr, "ok true\n");
+	else
+		fprintf(stderr, "ok not true\n");
 	fprintf(stderr, "(%u) insert_hash_reconfig added rcb: %p with %s\n", i, rcb, print_ss0(rcb->super));
 	return true;
 }
