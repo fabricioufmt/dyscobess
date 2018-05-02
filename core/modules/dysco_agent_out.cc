cc34576e8c577cba7418031984ac223cf03ebb98
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-01 22:50:56 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -356,6 +356,9 @@ DyscoHashOut* DyscoAgentOut::pick_path_ack(Tcp* tcp, DyscoHashOut* cb_out) {
 		fprintf(stderr, "pick_path_ack: going to new path\n");
 	else
 		fprintf(stderr, "pick_path_ack: going to old path\n");
+
+	fprintf(stderr, "cb_out->sup: %s\n", print_ss2(cb_out->sup));
+	fprintf(stderr, "cb_out->sub: %s\n", print_ss2(cb_out->sub));
 	
 	return cb;
 }
