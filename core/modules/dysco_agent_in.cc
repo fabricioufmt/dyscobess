3d79c6d7d223f7370374696d9f6d5ddc0b215787
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-06 22:09:56 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -103,7 +103,7 @@ void DyscoAgentIn::ProcessBatch(bess::PacketBatch* batch) {
 #endif
 
 		if(!isReconfigPacket(ip, tcp)) {
-			if(input(pkt, ip, tcp)) {
+			//if(input(pkt, ip, tcp)) {
 				out_gates[0].add(pkt);
 #ifdef DEBUG
 				fprintf(stderr, "[%s][DyscoAgentIn] forwards %s:%u -> %s:%u [%X:%X]\n\n",
@@ -111,7 +111,7 @@ void DyscoAgentIn::ProcessBatch(bess::PacketBatch* batch) {
 					printip1(ip->src.value()), tcp->src_port.value(),
 					printip1(ip->dst.value()), tcp->dst_port.value(),
 					tcp->seq_num.value(), tcp->ack_num.value());
-			}
+				//}
 #endif
 		} else {
 			switch(control_input(pkt, ip, tcp)) {
