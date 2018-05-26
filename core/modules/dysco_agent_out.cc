1a73938484d887778d449e7ece685d02dc161ca0
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 18:18:40 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -295,21 +295,26 @@ DyscoHashOut* DyscoAgentOut::pick_path_ack(Tcp* tcp, DyscoHashOut* cb_out) {
 			if(cb_out->use_np_ack) {
 				fprintf(stderr, "4\n");
 				cb = cb_out->other_path;
-			} else if(!dc->after(cb_out->ack_cutoff, ack)) {
-				fprintf(stderr, "5\n");
-				if(tcp->flags & Tcp::kFin)
-					cb = cb_out->other_path;
-				else {
-					fprintf(stderr, "6\n");
-					//TEST
-					//tcp->ack_num = be32_t(cb_out->ack_cutoff);
-					cb = cb_out->other_path;
-					cb_out->ack_ctr++;
-					if(cb_out->ack_ctr > 1) {
-						fprintf(stderr, "7\n");
-						cb_out->use_np_ack = 1;
+			} else {
+				fprintf(stderr, "cb_out->ack_cutoff: %X\n", cb_out->ack_cutoff);
+				fprintf(stderr, "ack               : %X\n", ack);
+				if(!dc->after(cb_out->ack_cutoff, ack)) {
+					fprintf(stderr, "5\n");
+					if(tcp->flags & Tcp::kFin)
+						cb = cb_out->other_path;
+					else {
+						fprintf(stderr, "6\n");
+						//TEST
+						//tcp->ack_num = be32_t(cb_out->ack_cutoff);
+						cb = cb_out->other_path;
+						cb_out->ack_ctr++;
+						if(cb_out->ack_ctr > 1) {
+							fprintf(stderr, "7\n");
+							cb_out->use_np_ack = 1;
+						}
 					}
-				}
+				} else
+					fprintf(stderr, "5,5\n");
 			} else
 				fprintf(stderr, "8\n");
 		} else
