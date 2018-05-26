0b1557cdc17bfdfbbe7f8f8189bf209940fe4922
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 17:00:55 -0400

    .

--- core/modules/dysco_util.h
+++ core/modules/dysco_util.h
@@ -530,16 +530,16 @@ inline void hdr_rewrite_csum(Ipv4* ip, Tcp* tcp, DyscoTcpSession* ss) {
 	hdr_rewrite(ip, tcp, ss);
 }
 
-inline void hdr_rewrite_full_csum(Ipv4* ip, Tcp* tcp, DyscoTcpSession* ss) {
-	hdr_rewrite(ip, tcp, ss);
-	fix_csum(ip, tcp);
-}
-
 inline void fix_csum(Ipv4* ip, Tcp* tcp) {
 	ip->checksum = 0;
 	tcp->checksum = 0;
 	ip->checksum = CalculateIpv4Checksum(*ip);
 	tcp->checksum = CalculateIpv4TcpChecksum(*ip, *tcp);	
+}
+
+inline void hdr_rewrite_full_csum(Ipv4* ip, Tcp* tcp, DyscoTcpSession* ss) {
+	hdr_rewrite(ip, tcp, ss);
+	fix_csum(ip, tcp);
 }
 
 /*********************************************************************
