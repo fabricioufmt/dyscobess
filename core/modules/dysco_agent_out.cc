214fd2c4573fa678b11b8ae380b29c500ce79b75
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 15:55:10 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -94,6 +94,15 @@ void DyscoAgentOut::ProcessBatch(PacketBatch* batch) {
 		} else
 			out_gates[0].add(pkt);
 
+		uint16_t check1 = tcp->checksum;
+		uint16_t check2 = bess::utils::CalculateIpv4TcpChecksum(*ip, *tcp);
+
+		//if(check1 != check2) {
+			fprintf(stderr, "[%s][DyscoAgentOut] tcp->checksum is: %X\n", ns.c_str(), check1);
+			fprintf(stderr, "[%s][DyscoAgentOut] checksum should : %X\n", ns.c_str(), check2);
+			fprintf(stderr, "[%s][DyscoAgentOut] forwarding %s [%X:%X]\n\n", ns.c_str(), printPacketSS(ip, tcp), tcp->seq_num.value(), tcp->ack_num.value());
+			//}
+		
 #ifdef DEBUG
 		fprintf(stderr, "[%s][DyscoAgentOut] forwards %s [%X:%X]\n\n", ns.c_str(), printPacketSS(ip, tcp), tcp->seq_num.value(), tcp->ack_num.value());
 #endif
