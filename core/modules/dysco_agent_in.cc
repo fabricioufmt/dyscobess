e095db9f898fa43d75e7b075032895cb487b3b6a
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 17:23:54 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -419,6 +419,11 @@ CONTROL_RETURN DyscoAgentIn::input(Packet* pkt, Ipv4* ip, Tcp* tcp, DyscoHashIn*
 		if(isTCPACK(tcp)) {
 			dc->set_ack_number_out(this->index, tcp, cb_in);
 			in_hdr_rewrite_csum(ip, tcp, cb_in);
+
+			if(cb_in->dcb_out->state == DYSCO_SYN_RECEIVED) {
+				fprintf(stderr, "Changed from SYN_RECEIVED to ESTABLISHED.\n");
+				cb_in->dcb_out->state = DYSCO_ESTABLISHED;
+			}
 		} else {
 			//It is retransmission packet, just remove sc (if there is) and insert Dysco Tag
 			if(payload_sz) {
