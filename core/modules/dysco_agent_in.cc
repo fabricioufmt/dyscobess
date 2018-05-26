4db1edce0f975a3652812e555fe71a16fb8a0476
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 17:30:59 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -414,15 +414,21 @@ CONTROL_RETURN DyscoAgentIn::input(Packet* pkt, Ipv4* ip, Tcp* tcp, DyscoHashIn*
 		
 		return TO_GATE_0;
 	}
+
+	if(isTCPACK(tcp)) {
+		if(cb_in->dcb_out->state == DYSCO_SYN_RECEIVED) {
+			fprintf(stderr, "Changed from SYN_RECEIVED to ESTABLISHED.\n");
+			cb_in->dcb_out->state = DYSCO_ESTABLISHED;
+		}
+	}
 	
 	if(isTCPSYN(tcp)) {
 		if(isTCPACK(tcp)) {
 			dc->set_ack_number_out(this->index, tcp, cb_in);
 			in_hdr_rewrite_csum(ip, tcp, cb_in);
 
-			fprintf(stderr, "state; %d\n", cb_in->dcb_out->state);
-			if(cb_in->dcb_out->state == DYSCO_SYN_RECEIVED) {
-				fprintf(stderr, "Changed from SYN_RECEIVED to ESTABLISHED.\n");
+			if(cb_in->dcb_out->state == DYSCO_SYN_SENT) {
+				fprintf(stderr, "Changed from SYN_SENT to ESTABLISHED.\n");
 				cb_in->dcb_out->state = DYSCO_ESTABLISHED;
 			}
 		} else {
