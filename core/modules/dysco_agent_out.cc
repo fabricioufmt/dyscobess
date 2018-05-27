e42ab4c8805869efa77b5a0945d1d1211e4c0496
fabricioufmt <fabricio.ufmt@gmail.com>
2018-05-26 21:31:44 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -305,9 +305,8 @@ DyscoHashOut* DyscoAgentOut::pick_path_ack(Tcp* tcp, DyscoHashOut* cb_out) {
 					}
 				}
 			}
-		}
-			//} else
-			//fprintf(stderr, "valid_ack_cut is FALSE(other_path->state: %d)\n", cb_out->other_path->state);
+		} else
+			fprintf(stderr, "valid_ack_cut is FALSE\n");
 	}
 	
 	return cb;
