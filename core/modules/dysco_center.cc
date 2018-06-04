c21d658bed5f9d40a3b8d19d5666f7c843321b8f
fabricioufmt <fabricio.ufmt@gmail.com>
2018-06-04 16:00:09 -0400

    .

--- core/modules/dysco_center.cc
+++ core/modules/dysco_center.cc
@@ -147,15 +147,13 @@ DyscoHashIn* DyscoCenter::lookup_input_by_ss(uint32_t i, DyscoTcpSession* ss) {
 	if(!dh)
 		return 0;
 
-	/*
 	unordered_map<DyscoTcpSession, DyscoHashIn*, DyscoTcpSessionHash>::iterator it = dh->hash_in.find(*ss);
 	if(it != dh->hash_in.end())
 		return it->second;
 	
 	return 0;
-	*/
 
-	return dh->hash_in[*ss];
+	//return dh->hash_in[*ss];
 }
 
 DyscoHashOut* DyscoCenter::lookup_output(uint32_t i, Ipv4* ip, Tcp* tcp) {
@@ -174,15 +172,13 @@ DyscoHashOut* DyscoCenter::lookup_output_by_ss(uint32_t i, DyscoTcpSession* ss) 
 	if(!dh)
 		return 0;
 
-	/*
 	unordered_map<DyscoTcpSession, DyscoHashOut*, DyscoTcpSessionHash>::iterator it = dh->hash_out.find(*ss);
 	if(it != dh->hash_out.end())
 		return it->second;
 	
 	return 0;
-	*/
 
-	return dh->hash_out[*ss];
+	//return dh->hash_out[*ss];
 }
 
 DyscoHashOut* DyscoCenter::lookup_output_pending(uint32_t i, Ipv4* ip, Tcp* tcp) {
@@ -253,15 +249,13 @@ DyscoHashOut* DyscoCenter::lookup_pending_tag_by_tag(uint32_t i, uint32_t tag) {
 	if(!dh)
 		return 0;
 
-	/*
 	unordered_map<uint32_t, DyscoHashOut*>::iterator it = dh->hash_pen_tag.find(tag);
 	if(it != dh->hash_pen_tag.end())
 		return it->second;
 	
 	return 0;
-	*/
 
-	return dh->hash_pen_tag[tag];
+	//return dh->hash_pen_tag[tag];
 }
 
 
@@ -270,15 +264,13 @@ DyscoCbReconfig* DyscoCenter::lookup_reconfig_by_ss(uint32_t i, DyscoTcpSession*
 	if(!dh)
 		return 0;
 
-	/*
 	unordered_map<DyscoTcpSession, DyscoCbReconfig*, DyscoTcpSessionHash, DyscoTcpSessionEqualTo>::iterator it = dh->hash_reconfig.find(*ss);
 	if(it != dh->hash_reconfig.end())
 		return it->second;
 	
 	return 0;
-	*/
 
-	return dh->hash_reconfig[*ss];
+	//return dh->hash_reconfig[*ss];
 }
 
 
