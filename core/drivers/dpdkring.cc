39e01835dcd11b81c7400cdaf7dcdd0f8169529c
fabricioufmt <fabricio.ufmt@gmail.com>
2018-08-05 19:04:17 -0400

    .

--- core/drivers/dpdkring.cc
+++ core/drivers/dpdkring.cc
@@ -4,17 +4,17 @@
 #include "../utils/format.h"
 
 CommandResponse DPDKRing::Init(const bess::pb::DPDKRingArg& arg) {
-	_tx_ring = rte_ring_lookup(arg._TX_RING().c_str());
+	_tx_ring = rte_ring_lookup(arg.TX_RING().c_str());
 	if(!_tx_ring) {
 		return CommandFailure(EINVAL, "Error to lookup the TX ring.");
 	}
 
-	_rx_ring = rte_ring_lookup(arg._RX_RING().c_str());
+	_rx_ring = rte_ring_lookup(arg.RX_RING().c_str());
 	if(!_rx_ring) {
 		return CommandFailure(EINVAL, "Error to lookup the RX ring.");
 	}
 	
-	_message_pool = rte_mempool_lookup(arg._MEM_POOL().c_str());
+	_message_pool = rte_mempool_lookup(arg.MEM_POOL().c_str());
 	if(!_message_pool) {
 		return CommandFailure(EINVAL, "Error to lookup the mempool.");
 	}
@@ -23,9 +23,9 @@ CommandResponse DPDKRing::Init(const bess::pb::DPDKRingArg& arg) {
 }
 
 int DPDKRing::RecvPackets(queue_t, bess::Packet** pkts, int cnt) {
-	return rte_ring_dequeue_burst(_rx_ring, (struct mbuf**) pkts, cnt);
+	return rte_ring_dequeue_burst(_rx_ring, (void*) pkts, cnt);
 }
 
 int DPDKRing::SendPackets(queue_t, bess::Packet** pkts, int cnt) {
-	return rte_ring_enqueue_burst(_tx_ring, (struct mbuf**) pkts, cnt);
+	return rte_ring_enqueue_burst(_tx_ring, (void*) pkts, cnt);
 }
