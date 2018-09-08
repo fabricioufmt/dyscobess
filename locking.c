cf63a4509b2172442c5cf5ce7d18c4f848055032
fabricioufmt <fabricio.ufmt@gmail.com>
2018-09-08 16:13:59 -0400

    .

--- locking.c
+++ locking.c
@@ -25,6 +25,46 @@
 #define OPT_KIND 254
 #define OPT_LENGTH 4
 
+struct tcp_session {
+	uint32_t sip;
+	uint32_t dip;
+	uint16_t sport;
+	uint16_t dport;
+};
+
+struct reconfig_message {
+	struct tcp_session      my_sub;
+	struct tcp_session	super;
+	struct tcp_session	leftSS;
+	struct tcp_session	rightSS;
+	uint32_t		leftA;
+	uint32_t		rightA;
+	
+        uint16_t		sport;
+	uint16_t		dport;
+	
+	uint32_t		leftIseq;
+	uint32_t		leftIack;
+	
+	uint32_t		rightIseq;
+	uint32_t		rightIack;
+	
+	uint32_t		seqCutoff;
+	
+	uint32_t		leftIts;
+	uint32_t		leftItsr;
+	
+	uint16_t		leftIws;
+	uint16_t		leftIwsr;
+
+	uint16_t		sackOk;
+	
+	uint16_t		semantic;
+	
+	uint32_t		srcMB;
+	uint32_t		dstMB;
+} __attribute__((packed));
+
 struct pseudo_header {
 	uint32_t source_address;
 	uint32_t dest_address;
@@ -46,7 +86,7 @@ struct tcpopt {
 
 uint16_t csum(uint16_t*, uint32_t);
 void create_datagram(struct iphdr*, uint32_t, uint32_t, uint32_t);
-void create_segment(struct iphdr*, struct tcphdr*, uint16_t, uint16_t, struct tcpopt*, uint8_t, uint8_t, uint32_t, uint32_t*);
+void create_segment(struct iphdr*, struct tcphdr*, uint16_t, uint16_t, struct tcpopt*, uint8_t, uint8_t, struct reconfig_message*, uint32_t, uint32_t*);
 
 int main(int argc, char** argv) {
 	//Variables
@@ -61,6 +101,7 @@ int main(int argc, char** argv) {
 	struct tcpopt* tcpo;
 	struct sockaddr_in sin;
 	uint8_t buffer[BUFSIZE];
+	struct reconfig_message* cmsg;
 	
 	if(argc < MIN_ARGC) {
 		fprintf(stderr, "Usage: %s <SIP> <SP> <DIP> <DP> <left_hop> <right_hop> [service chain].\n", argv[0]);
@@ -89,6 +130,9 @@ int main(int argc, char** argv) {
 	packet_len += sizeof(struct tcphdr);
 	tcpo = (struct tcpopt*) (buffer + packet_len);
 	packet_len += OPT_LENGTH;
+
+	cmsg = (struct reconfig_message*) (buffer + packet_len);
+	packet_len += sizeof(struct reconfig_message);
 	
 	sc_len = argc - MIN_ARGC;
 	uint32_t* sc = (uint32_t*) malloc(sc_len * 4);
@@ -99,7 +143,7 @@ int main(int argc, char** argv) {
 	packet_len += sc_len * sizeof(uint32_t);
 	
 	create_datagram(iph, inet_addr(argv[1]), inet_addr(argv[3]), packet_len);
-	create_segment(iph, tcph, htons(atoi(argv[2])), htons(atoi(argv[4])), tcpo, atoi(argv[5]), atoi(argv[6]), sc_len, sc);
+	create_segment(iph, tcph, htons(atoi(argv[2])), htons(atoi(argv[4])), tcpo, atoi(argv[5]), atoi(argv[6]), cmsg, sc_len, sc);
 
 	if((n = sendto(sockfd, buffer, ntohs(iph->tot_len), 0, (struct sockaddr*) &sin, sizeof(sin))) < 0) {
 		fprintf(stderr, "ERROR: %s.\n", strerror(errno));
@@ -125,7 +169,7 @@ void create_datagram(struct iphdr* iph, uint32_t sip, uint32_t dip, uint32_t pac
 	iph->check = csum((uint16_t*) iph, iph->ihl << 2);
 }
 
-void create_segment(struct iphdr* iph, struct tcphdr* tcph, uint16_t sport, uint16_t dport, struct tcpopt* tcpo, uint8_t lhop, uint8_t rhop, uint32_t sc_len, uint32_t* sc) {
+void create_segment(struct iphdr* iph, struct tcphdr* tcph, uint16_t sport, uint16_t dport, struct tcpopt* tcpo, uint8_t lhop, uint8_t rhop, struct reconfig_message* cmsg, uint32_t sc_len, uint32_t* sc) {
 	// Construct the TCP segment 
 	tcph->source = sport;
 	tcph->dest = dport;
@@ -155,13 +199,14 @@ void create_segment(struct iphdr* iph, struct tcphdr* tcph, uint16_t sport, uint
 	psh.dest_address = iph->daddr;
 	psh.placeholder = 0;
 	psh.protocol = IPPROTO_TCP;
-	psh.tcp_length = htons((tcph->doff << 2) + sc_len * sizeof(uint32_t));
-	uint32_t size = sizeof(struct pseudo_header) + (tcph->doff << 2) + sc_len * sizeof(uint32_t);
+	psh.tcp_length = htons((tcph->doff << 2) + sizeof(struct reconfig_message) + sc_len * sizeof(uint32_t));
+	uint32_t size = sizeof(struct pseudo_header) + (tcph->doff << 2) + sizeof(struct reconfig_message) + sc_len * sizeof(uint32_t);
 	uint8_t* buff = malloc(size);
 
 	memcpy(buff, &psh, sizeof(struct pseudo_header));
 	memcpy(buff + sizeof(struct pseudo_header), tcph, tcph->doff << 2);
-	memcpy(buff + sizeof(struct pseudo_header) + (tcph->doff << 2), sc, sc_len * sizeof(uint32_t));
+	memcpy(buff + sizeof(struct pseudo_header) + (tcph->doff << 2), cmsg, sizeof(struct reconfig_message));
+	memcpy(buff + sizeof(struct pseudo_header) + (tcph->doff << 2) + sizeof(struct reconfig_message), sc, sc_len * sizeof(uint32_t));
 	tcph->check = csum((uint16_t*) buff, size);
 }
 
