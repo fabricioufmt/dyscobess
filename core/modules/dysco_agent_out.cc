d6e45b059379d16ce5e1882183d072766b17cc3b
fabricioufmt <fabricio.ufmt@gmail.com>
2018-09-09 16:13:50 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -630,6 +630,12 @@ bool DyscoAgentOut::output_mb(Packet* pkt, Ipv4* ip, Tcp* tcp, DyscoHashOut* cb_
 	
 	hdr_rewrite(ip, tcp, &cb_out->sub);
 
+	memcpy(&cb_out->mac_sub, pkt->head_data<Ethernet*>(), sizeof(Ethernet));
+
+	fprintf(stderr, "copying MAC values(output)\n");
+	fprintf(stderr, "MAC SRC: %s\n", cb_out->mac_sub.src_addr.ToString().c_str());
+	fprintf(stderr, "MAC DST: %s\n", cb_out->mac_sub.dst_addr.ToString().c_str());
+	
 	if(cb_out->tag_ok) {
 		remove_tag(pkt, ip, tcp);
 	}
