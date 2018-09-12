4c43fdbbdbb1ff912a2ffd80eafe187f5b98f5de
fabricioufmt <fabricio.ufmt@gmail.com>
2018-09-11 23:05:16 -0400

    .

--- core/modules/dysco_util.h
+++ core/modules/dysco_util.h
@@ -663,7 +663,9 @@ inline DyscoTcpOption* isLockSignalPacket(Tcp* tcp) {
 	return 0;
 }
 
-inline bool isLockingPacket(Ipv4* ip, Tcp* tcp) {
+inline bool isLockingPacket(Ipv4*, Tcp*) {
+//inline bool isLockingPacket(Ipv4* ip, Tcp* tcp) {
+	/*
 	uint32_t payload_len = hasPayload(ip, tcp);
 	if(isTCPSYN(tcp) && payload_len) {
 		if(payload_len > sizeof(DyscoControlMessage)) {
@@ -671,7 +673,7 @@ inline bool isLockingPacket(Ipv4* ip, Tcp* tcp) {
 			return cmsg->type == DYSCO_LOCK;
 		}
 	}
-
+	*/
 	return false;
 }
 
