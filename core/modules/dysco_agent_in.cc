851e742dbf6e5ade8a2fee092477df21b7a0fb77
fabricioufmt <fabricio.ufmt@gmail.com>
2018-09-14 14:15:53 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -1972,7 +1972,7 @@ CONTROL_RETURN DyscoAgentIn::processAckLock(Packet* pkt, Ipv4* ip, Tcp* tcp, Dys
 	return ERROR;
 }
 
-void DyscoAgentIn::start_reconfiguration(Packet*, Ipv4* ip, Tcp* tcp, DyscoControlMessage* cmsg, DyscoHashIn* cb_in) {
+void DyscoAgentIn::start_reconfiguration(Packet*, Ipv4*, Tcp*, DyscoControlMessage* cmsg, DyscoHashIn* cb_in) {
 
 	//uint32_t* sc = reinterpret_cast<uint32_t*>(cmsg + 1);
 	//uint32_t sc_len = (hasPayload(ip, tcp) - sizeof(DyscoControlMessage))/sizeof(uint32_t);
