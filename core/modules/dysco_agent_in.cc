3ff19ce9038c8f25aeadaccf342e994041c16ab8
fabricioufmt <fabricio.ufmt@gmail.com>
2018-09-23 23:02:04 -0400

    .

--- core/modules/dysco_agent_in.cc
+++ core/modules/dysco_agent_in.cc
@@ -1616,8 +1616,8 @@ Packet* DyscoAgentIn::createLockingPacket(Packet* pkt, Ethernet* eth, Ipv4* ip, 
 	cmsg->super.dport = cb_in->my_sup.sport;
 	cmsg->neigh_sub.sip = tcpo->tag;
 	cmsg->neigh_sub.sport = tcpo->sport;
-	cmsg->neigh_sub.dip = cb_in->my_sup.dip;
-	cmsg->neigh_sub.dport = cb_in->my_sup.dport;
+	cmsg->neigh_sub.dip = cb_in->my_sup.sip;
+	cmsg->neigh_sub.dport = cb_in->my_sup.sport;
 	cmsg->lhop = rhop;
 	cmsg->rhop = rhop;
 
