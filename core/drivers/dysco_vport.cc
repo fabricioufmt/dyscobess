8e2ade43678dd2f0ba01509d86f6becfa56cada5
fabricioufmt <fabricio.ufmt@gmail.com>
2018-10-02 15:28:51 -0400

    .

--- core/drivers/dysco_vport.cc
+++ core/drivers/dysco_vport.cc
@@ -589,11 +589,13 @@ int DyscoVPort::SendPackets(queue_t qid, bess::Packet **pkts, int cnt) {
 		bess::Packet *snb = pkts[i];
 
 		struct sn_rx_desc *rx_desc;
-
+		//TEST
+		fprintf(stderr, "snb1=%p\n", snb);
 		rx_desc = snb->scratchpad<struct sn_rx_desc *>();
 
 		rte_prefetch0(rx_desc);
-
+		//TEST
+		fprintf(stderr, "snb2=%p\n", snb);
 		paddr[i] = snb->paddr();
 	}
 
