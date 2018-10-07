c47762a70f5001acec300a7845108d07a91e5328
fabricioufmt <fabricio.ufmt@gmail.com>
2018-10-07 15:22:14 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -384,13 +384,13 @@ DyscoHashOut* DyscoAgentOut::pick_path_ack(Tcp* tcp, DyscoHashOut* cb_out) {
 		fprintf(stderr, "cb_out->sup: %s -- cb_out->sub: %s\n", printSS(cb_out->sup), printSS(cb_out->sub));
 		fprintf(stderr, "cb_out->valid_ack_cut: %d\n", cb_out->valid_ack_cut);
 		fprintf(stderr, "cb_out->use_np_ack: %d\n", cb_out->use_np_ack);
-		fprintf(stderr, "after(cb_out->ack_cutoff, ack)[%X, %X]: %d\n", cb_out->ack_cutoff, ack, after(cb_out->ack_cutoff, ack));
+		fprintf(stderr, "after(ack, cb_out->ack_cutoff)[%X, %X]: %d\n", cb_out->ack_cutoff, ack, after(ack, cb_out->ack_cutoff));
 #endif
 		if(cb_out->valid_ack_cut) {
 			if(cb_out->use_np_ack) {
 				cb = cb_out->other_path;
 			} else {
-				if(!after(cb_out->ack_cutoff, ack)) {
+				if(after(ack, cb_out->ack_cutoff)) {
 					if(isTCPFIN(tcp))
 						cb = cb_out->other_path;
 					else {
@@ -399,8 +399,8 @@ DyscoHashOut* DyscoAgentOut::pick_path_ack(Tcp* tcp, DyscoHashOut* cb_out) {
 #endif
 						cb = cb_out->other_path;
 						cb_out->ack_ctr++;
-						if(cb_out->ack_ctr > 1)
-							cb_out->use_np_ack = 1;
+						//if(cb_out->ack_ctr > 1)
+						//	cb_out->use_np_ack = 1;
 					}
 				} else {
 #ifdef DEBUG_RECONFIG
