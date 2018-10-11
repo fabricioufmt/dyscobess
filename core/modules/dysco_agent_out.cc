b0b9e92ae2e7abf691afbb7b2366be4df8b424a5
fabricioufmt <fabricio.ufmt@gmail.com>
2018-10-11 09:59:07 -0400

    .

--- core/modules/dysco_agent_out.cc
+++ core/modules/dysco_agent_out.cc
@@ -1039,7 +1039,7 @@ bool DyscoAgentOut::processLockingSignalPacket(Packet* pkt, Ethernet* eth, Ipv4*
 
 bool DyscoAgentOut::forward(Packet* pkt, bool reliable) {
 	if(!reliable) {
-		fprintf(stderr, "forwarding without reliable\n");
+		fprintf(stderr, "[%s]forwarding without reliable\n", ns.c_str());
 		PacketBatch out_gate;
 		out_gate.clear();
 		out_gate.add(pkt);
