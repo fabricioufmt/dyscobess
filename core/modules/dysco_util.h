27cad26ffa147da272b5b6fbf28fac09c9e0b16e
fabricioufmt <fabricio.ufmt@gmail.com>
2018-10-11 16:00:02 -0400

    .

--- core/modules/dysco_util.h
+++ core/modules/dysco_util.h
@@ -721,7 +721,6 @@ inline bool isLockingSignalPacket(Tcp* tcp) {
 
 inline bool isLockingPacket(Ipv4* ip, Tcp* tcp) {
 	uint32_t tcp_hlen = tcp->offset << 2;
-	uint32_t ip_tlen = ip->length().value();
 	uint32_t payload_len = hasPayload(ip, tcp);
 
 	if(isTCPSYN(tcp) && payload_len) {
