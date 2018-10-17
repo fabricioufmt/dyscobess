86f4a4dd6f935a55840220a93aa4194856d0c14e
Fabricio <fabricioufmt@gmail.com>
2018-10-17 13:14:39 -0400

    .

--- stop.sh
+++ stop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "killing iperf"
 sudo pkill -9 iperf
+echo "killing proxy"
 sudo pkill -9 tcp_proxy
-sudo pkill -9 tcpdump
+echo "killing tcpdump"
+sudo pkill tcpdump
+
+echo "stopping BESS daemon..."
+sudo ./bessctl/bessctl daemon stop
