c7270c560d30f199bb58658ac1b81fb17a0b2754
Fabricio <fabricioufmt@gmail.com>
2018-10-25 12:24:22 +0000

    .

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export RTE_SDK=$PWD/deps/dpdk-17.05
-export RTE_TARGET=x86_64-native-linuxapp-gcc
+echo "calling stop.sh..."
+sh stop.sh
+
 HUGEPGSZ=`cat /proc/meminfo | grep Hugepagesize | cut -d: -f2 | tr -d ' '`
 
 echo "removing reserved hugepages"
@@ -15,8 +16,8 @@ rm -rf .echo_tmp
 sudo umount /mnt/huge 1>/dev/null 2>/dev/null
 sudo rm -R /mnt/huge 1>/dev/null 2>/dev/null
 
-echo "reserving 1024 hugepages(2048kB)"
-echo "echo 1024 > /sys/kernel/mm/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" > .echo_tmp
+echo "reserving 2048 hugepages(2048kB)"
+echo "echo 2048 > /sys/kernel/mm/hugepages/hugepages-${HUGEPGSZ}/nr_hugepages" > .echo_tmp
 sudo sh .echo_tmp
 rm -rf .echo_tmp
 
@@ -27,6 +28,6 @@ sudo mount -t hugetlbfs nodev /mnt/huge
 echo "removing bess module..."
 sudo rmmod bess 1>/dev/null 2>/dev/null
 echo "starting BESS daemon"
-sudo ./bessctl/bessctl daemon start
+sudo ./bessctl/bessctl daemon start -m 4096
 echo "running ex2 script file..."
 sudo ./bessctl/bessctl run ex2
